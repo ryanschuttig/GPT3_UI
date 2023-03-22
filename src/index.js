9d8856c0359361a1623a18d0d264372f8b1603b4
ryanschuttig <ryanschuttig@gmail.com>
2023-03-21 22:57:20 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// render app inside div with ID of "root" index.html
+ReactDOM.render(<App />, document.getElementById("root"));
